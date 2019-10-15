5ea1408c14cf9f20784c6137b8df7575b0d3ec3c
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-15 17:31:31 -0400

    yeah

---
+++ Database/DB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class DB
+    {
+        private static Demo1Entities myDb = new Demo1Entities();
+        public static IEnumerable<Product> GetProducts()
+        {
+            return myDb.Products.ToList();
+        }
+
+        public static Product GetProduct(int id)
+        {
+            return myDb.Products.Find(id);
+        }
+
+        public static Product AddProduct(Product p)
+        {
+            myDb.Products.Add(p);
+            myDb.SaveChanges();
+
+            return p;
+        }
+
+        public static Product UpdateProduct(int id, Product p)
+        {
+            myDb.Products.Find(id).ProductDescription = p.ProductDescription;
+            myDb.Products.Find(id).ProductUpc = p.ProductUpc;
+
+            return p;
+        }
+    }
+}
--- entityFrameworkDemo/Program.cs
+++ entityFrameworkDemo/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks; 
 
 namespace entityFrameworkDemo
 {
@@ -10,6 +11,16 @@ namespace entityFrameworkDemo
     {
         static void Main(string[] args)
         {
+            var addingProduct = new Product();
+            addingProduct.ProductDescription = "MacBook Air";
+            addingProduct.ProductUpc = "5524008365214";
+            var newId = DB.AddProduct(addingProduct).ProductId;
+            Console.WriteLine("Product added with id: " + newId);
+
+
+
+            var myProduct = Database.DB.GetProduct(1);
+            Console.WriteLine(myProduct.ProductDescription.ToString());
         }
     }
 }
